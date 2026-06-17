dc373290680db26b2a41ee4f31a631b4c9565884
zdni <alzidni.kasim@gmail.com>
2026-06-17 16:37:55 +0700

    feat: get direction in query dashboard_engine_item

--- server.js
+++ server.js
@@ -168,7 +168,7 @@ io.on('connection', (socket) => {
 
       // Jika lolos sekuritas, ambil komponen item grafik di dalam halaman tersebut
       const itemsQuery = `
-        SELECT id, name, chart_type, allow_toggle_view, query, xaxis_value_type, yaxis_value_type, icon 
+        SELECT id, name, chart_type, allow_toggle_view, query, xaxis_value_type, yaxis_value_type, icon, direction 
         FROM dashboard_engine_item 
         WHERE page_id = $1 
         ORDER BY sequence, id
@@ -198,7 +198,7 @@ io.on('connection', (socket) => {
       console.log(`[Data Engine] Menerima request data untuk Item ID: ${itemId}`);
 
       const itemQuery = `
-        SELECT id, name, chart_type, query, xaxis_value_type, yaxis_value_type, icon 
+        SELECT id, name, chart_type, query, xaxis_value_type, yaxis_value_type, icon, direction 
         FROM dashboard_engine_item
         WHERE id = $1 LIMIT 1
       `;
